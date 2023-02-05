236de8aca2c0dca5b1601239f04f9dd7a39ff656
Arcanox <arcanox@arcanox.me>
2023-02-05 02:16:41 -0600

    Swap out "broken rando" test BMSSV files and add a unit test that can quickly fix a "broken power bombs" rando file (for quality of life purposes)

--- test/MercuryEngine.Data.Test/BmssvTests.cs
+++ test/MercuryEngine.Data.Test/BmssvTests.cs
@@ -35,6 +35,30 @@ public partial class BmssvTests
 	private static string GetTestProfilePath(string profileName)
 		=> Path.Combine(TestContext.CurrentContext.TestDirectory, "TestFiles", "BMSSV", profileName);
 
+	[TestCase]
+	public void FixSaveFile()
+	{
+		string saveFolder = GetTestProfilePath(TestRandoBrokenProfile);
+		string saveFile = Path.Combine(saveFolder, "common.bmssv");
+		using var fileStream = File.Open(saveFile, FileMode.Open, FileAccess.Read);
+		using var outStream = File.Open(saveFile.Replace(".bmssv", "_fixed.bmssv"), FileMode.Create, FileAccess.Write);
+		var bmssv = new Bmssv();
+
+		try
+		{
+			bmssv.Read(fileStream);
+
+			var inventory = bmssv.Sections["PLAYER_INVENTORY"];
+
+			inventory.PutValue("ITEM_WEAPON_POWER_BOMB", 1f);
+			bmssv.Write(outStream);
+		}
+		finally
+		{
+			DumpBmssvFile(bmssv, saveFile);
+		}
+	}
+
 	[TestCaseSource(nameof(GetTestFiles))]
 	public void TestLoadBmssv(string inFile)
 	{
@@ -48,35 +72,7 @@ public partial class BmssvTests
 		}
 		finally
 		{
-			var serializer = new JsonSerializer {
-				Formatting = Formatting.Indented,
-				ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-				Converters = {
-					new StringEnumConverter(),
-					new TerminatedStringConverter(),
-				},
-			};
-			var jsonObject = JObject.FromObject(bmssv, serializer);
-
-			jsonObject.Sort();
-
-			using var textWriter = new StringWriter();
-			using var jsonWriter = new JsonTextWriter(textWriter);
-			
-			serializer.Serialize(jsonWriter, jsonObject);
-
-			var jsonDump = textWriter.ToString();
-
-			TestContext.Out.WriteLine("JSON dump of current parsed state:");
-			TestContext.Out.WriteLine(jsonDump);
-
-			var outFileDir = Path.GetDirectoryName(filePath)!;
-			var outFileName = Path.GetFileNameWithoutExtension(filePath) + ".json";
-			var outFilePath = Path.Combine(outFileDir, outFileName);
-			using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
-			using var writer = new StreamWriter(outFileStream, Encoding.UTF8);
-
-			writer.Write(jsonDump);
+			DumpBmssvFile(bmssv, filePath);
 		}
 	}
 
@@ -111,5 +107,38 @@ public partial class BmssvTests
 
 		for (var i = 0; i < newBuffer.Length; i++)
 			Assert.That(newBuffer[i], Is.EqualTo(originalBuffer[i]), $"Data mismatch at offset {i}");
+	}
+
+	private static void DumpBmssvFile(Bmssv bmssv, string bmssvFilePath)
+	{
+		var serializer = new JsonSerializer {
+			Formatting = Formatting.Indented,
+			ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+			Converters = {
+				new StringEnumConverter(),
+				new TerminatedStringConverter(),
+			},
+		};
+		var jsonObject = JObject.FromObject(bmssv, serializer);
+
+		jsonObject.Sort();
+
+		using var textWriter = new StringWriter();
+		using var jsonWriter = new JsonTextWriter(textWriter);
+
+		serializer.Serialize(jsonWriter, jsonObject);
+
+		var jsonDump = textWriter.ToString();
+
+		TestContext.Out.WriteLine("JSON dump of current parsed state:");
+		TestContext.Out.WriteLine(jsonDump);
+
+		var outFileDir = Path.GetDirectoryName(bmssvFilePath)!;
+		var outFileName = Path.GetFileNameWithoutExtension(bmssvFilePath) + ".json";
+		var outFilePath = Path.Combine(outFileDir, outFileName);
+		using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
+		using var writer = new StreamWriter(outFileStream, Encoding.UTF8);
+
+		writer.Write(jsonDump);
 	}
 }
