fd487a4733fb8fb49c663d546de16b6d0b90d515
Arcanox <arcanox@arcanox.me>
2024-09-25 18:15:35 -0500

    Add async BMSAD tests as well as a single test method that dumps all BMSADs in one go (to test performance and caching)

--- test/MercuryEngine.Data.Tests/BmsadTests.Async.cs
+++ test/MercuryEngine.Data.Tests/BmsadTests.Async.cs
@@ -1,73 +1,58 @@
-﻿using System.Diagnostics;
+﻿using MercuryEngine.Data.Core.Framework.Mapping;
+using MercuryEngine.Data.Formats;
+using MercuryEngine.Data.Tests.Utility.Json;
+using MercuryEngine.Data.Tests.Utility;
+using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
-using MercuryEngine.Data.Core.Framework.Mapping;
-using MercuryEngine.Data.Formats;
 using MercuryEngine.Data.Tests.Extensions;
-using MercuryEngine.Data.Tests.Utility;
-using MercuryEngine.Data.Tests.Utility.Json;
 
 namespace MercuryEngine.Data.Tests;
 
-[TestFixture]
 public partial class BmsadTests
 {
-	private static readonly string PackagesPath = Configuration.PackagesPath; // Store it for faster access
-
-	private static IEnumerable<TestCaseData> GetTestFiles()
-	{
-		foreach (var bmsadFile in Directory.EnumerateFiles(PackagesPath, "*.bmsad", SearchOption.AllDirectories))
-		{
-			var relativePath = Path.GetRelativePath(PackagesPath, bmsadFile);
-
-			yield return new TestCaseData(bmsadFile, false) {
-				TestName = relativePath,
-			};
-		}
-	}
-
 	[Test]
-	public void TestDumpAllBmsads()
+	public async Task TestDumpAllBmsadsAsync()
 	{
 		var files = Directory.EnumerateFiles(PackagesPath, "*.bmsad", SearchOption.AllDirectories).ToList();
 		var sw = new Stopwatch();
 
-		TestContext.Progress.Write($"Dumping {files.Count} BMSAD files...");
+		await TestContext.Progress.WriteAsync($"Dumping {files.Count} BMSAD files...");
 		sw.Start();
 
 		foreach (var file in files)
-			TestLoadBmsad(file, quiet: true);
+			await TestLoadBmsadAsync(file, quiet: true);
 
 		sw.Stop();
-		TestContext.Progress.WriteLine($"Done! Took {sw.Elapsed}.");
+		await TestContext.Progress.WriteLineAsync($"Done! Took {sw.Elapsed}.");
 	}
 
 	[TestCaseSource(nameof(GetTestFiles)), Parallelizable]
-	public void TestLoadBmsad(string inFile, bool quiet = false)
+	public async Task TestLoadBmsadAsync(string inFile, bool quiet = false)
 	{
 		if (!quiet)
-			TestContext.Progress.WriteLine("Loading BMSAD file: {0}", inFile);
+			await TestContext.Progress.WriteLineAsync($"Loading BMSAD file: {inFile}");
 
-		using var fileStream = File.Open(inFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+		await using var fileStream = File.Open(inFile, FileMode.Open, FileAccess.Read, FileShare.Read);
 		var bmsad = new Bmsad();
 
 		try
 		{
-			bmsad.Read(fileStream);
+			await bmsad.ReadAsync(fileStream);
 		}
 		finally
 		{
-			DumpBmsadFile(bmsad, inFile, quiet);
+			await DumpBmsadFileAsync(bmsad, inFile, quiet);
 		}
 	}
 
 	[TestCaseSource(nameof(GetTestFiles)), Parallelizable]
-	public void TestCompareBmsad(string inFile, bool quiet = false)
+	public async Task TestCompareBmsadAsync(string inFile, bool quiet = false)
 	{
 		if (!quiet)
 			TestContext.Progress.WriteLine("Parsing BMSAD file: {0}", inFile);
 
-		using var fileStream = File.Open(inFile, FileMode.Open, FileAccess.Read);
+		await using var fileStream = File.Open(inFile, FileMode.Open, FileAccess.Read);
 		using var reader = new BinaryReader(fileStream);
 		var originalBuffer = reader.ReadBytes((int) fileStream.Length);
 
@@ -78,11 +63,11 @@ public partial class BmsadTests
 			DataMapper = dataMapper,
 		};
 
-		bmsad.Read(fileStream);
+		await bmsad.ReadAsync(fileStream);
 
 		using var tempStream = new MemoryStream();
 
-		bmsad.Write(tempStream);
+		await bmsad.WriteAsync(tempStream);
 
 		var relativePath = Path.GetDirectoryName(Path.GetRelativePath(PackagesPath, inFile))!;
 		var outFileDir = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestFiles", "BMSAD", relativePath);
@@ -90,15 +75,15 @@ public partial class BmsadTests
 		var outFilePath = Path.Combine(outFileDir, outFileName);
 
 		if (!quiet)
-			TestContext.Progress.WriteLine("Output BMSAD file: {0}", outFilePath);
+			await TestContext.Progress.WriteLineAsync($"Output BMSAD file: {outFilePath}");
 
 		DataUtilities.DumpDataMapper(dataMapper, outFilePath);
 		tempStream.Seek(0, SeekOrigin.Begin);
 
 		var newBuffer = tempStream.ToArray();
-		using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
+		await using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
 
-		tempStream.CopyTo(outFileStream);
+		await tempStream.CopyToAsync(outFileStream);
 
 		if (inFile.EndsWith("pf_mushr_fr.bmsad"))
 			Assert.Pass("The \"pf_mushr_fr.bmsad\" file has an encoding error in the base game, so comparison is skipped.");
@@ -115,15 +100,15 @@ public partial class BmsadTests
 			{
 				var failureRangePath = dataMapper.GetContainingRanges((ulong) i);
 
-				TestContext.Error.WriteLine("The pending data assertion failed at the following range in the written data:");
-				TestContext.Error.WriteLine(failureRangePath.FormatRangePath());
+				await TestContext.Error.WriteLineAsync("The pending data assertion failed at the following range in the written data:");
+				await TestContext.Error.WriteLineAsync(failureRangePath.FormatRangePath());
 
 				throw;
 			}
 		}
 	}
 
-	private static void DumpBmsadFile(Bmsad bmsad, string bmsadFilePath, bool quiet = false)
+	private static async Task DumpBmsadFileAsync(Bmsad bmsad, string bmsadFilePath, bool quiet = false)
 	{
 		var jsonObject = JsonSerializer.SerializeToNode(bmsad, JsonUtility.JsonOptions)!.AsObject();
 
@@ -133,8 +118,8 @@ public partial class BmsadTests
 
 		if (!quiet)
 		{
-			TestContext.Out.WriteLine("JSON dump of current parsed state:");
-			TestContext.Out.WriteLine(jsonDump);
+			await TestContext.Out.WriteLineAsync("JSON dump of current parsed state:");
+			await TestContext.Out.WriteLineAsync(jsonDump);
 		}
 
 		var relativePath = Path.GetDirectoryName(Path.GetRelativePath(PackagesPath, bmsadFilePath))!;
@@ -144,9 +129,9 @@ public partial class BmsadTests
 
 		Directory.CreateDirectory(outFileDir);
 
-		using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
-		using var writer = new StreamWriter(outFileStream, Encoding.UTF8);
+		await using var outFileStream = File.Open(outFilePath, FileMode.Create, FileAccess.Write);
+		await using var writer = new StreamWriter(outFileStream, Encoding.UTF8);
 
-		writer.Write(jsonDump);
+		await writer.WriteAsync(jsonDump);
 	}
 }
